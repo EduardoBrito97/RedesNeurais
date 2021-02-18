2545a634ce6559904c4e7a21a929515f1a172e1e
Lucas Pontes <lpa1@motorola.com>
2021-02-18 17:29:47 -0300

    .

--- MiniProjeto1/reconhecimento/src/evaluation.py
+++ MiniProjeto1/reconhecimento/src/evaluation.py
@@ -2,39 +2,42 @@ import mnist_loader
 import network
 import csv
 import sys
+from tqdm import tqdm
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 HIDDEN_LAYERS = [1, 2, 3, 4]
-HIDDEN_NEURONS = [30, 60, 120, 240]
-LEARNING_RATE = [0.001, 0.01, 0.1, 1.0]
+HIDDEN_NEURONS = [30, 60]
+LEARNING_RATE = [0.01, 0.1, 1.0]
 EPOCHS = 30
 BATCH_SIZE = [4, 8, 16]
+with tqdm(total=36) as tq:
+    for hn in HIDDEN_NEURONS:
+        net_input = []
+        net_input.append(784)
+        net_settings = []
 
-for hn in HIDDEN_NEURONS:
-    net_input = []
-    net_input.append(784)
-    net_settings = []
+        for hl in HIDDEN_LAYERS:
+            net_input.append(hn)
+            net_input.append(10)
 
-    for hl in HIDDEN_LAYERS:
-        net_input.append(hn)
-        net_input.append(10)
+            net = network.Network(net_input)
 
-        net = network.Network(net_input)
+            for lr in LEARNING_RATE:
+                for bs in BATCH_SIZE:
+                    acc = net.SGD(training_data, EPOCHS, bs, lr, test_data)
 
-        for lr in LEARNING_RATE:
-            for bs in BATCH_SIZE:
-                acc = net.SGD(training_data, EPOCHS, bs, lr, test_data)
+                    sett = {'Hidden Layers': hl,
+                            'Hidden Neurons': hn,
+                            'Learning Rate': lr,
+                            'Batch Size': bs,
+                            'Accuracy': acc}
 
-                sett = {'Hidden Layers': hl,
-                        'Hidden Neurons': hn,
-                        'Learning Rate': lr,
-                        'Batch Size': bs,
-                        'Accuracy': acc}
+                    net_settings.append(sett)
+                    tq.set_postfix(sett)
+                    tq.update()
+                    #print(sett)
 
-                net_settings.append(sett)
-                print(sett)
-            
         net_input = net_input[:-1]
 
 with open('results.csv', 'wb+') as dictwriter:
